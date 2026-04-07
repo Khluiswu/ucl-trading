3ba307d17915c19093f24e0b4c42fb18855fee29
Kaihao Luis Wu <khwu668@gmail.com>
2026-04-07 20:49:34 +0200

    fix pages

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="bg-black text-white p-10">
+      <h1 className="text-3xl">About</h1>
+      <p>UCL Trading Society</p>
+    </div>
+  );
+}
---
+++ app/events/page.tsx
@@ -0,0 +1,8 @@
+export default function Events() {
+  return (
+    <div className="bg-black text-white p-10">
+      <h1 className="text-3xl">Events</h1>
+      <p>UCL Trading Society</p>
+    </div>
+  );
+}
